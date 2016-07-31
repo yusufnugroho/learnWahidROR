b0963d9d6290385c97095a9d75d9cc53a3b59999
yusufnugroho <kotakkode@gmail.com>
2016-07-31 17:23:14 +0700

    Data Type (String, Fixnum, Bignum)

---
+++ learnBasic/5learnDataType.rb
@@ -0,0 +1,24 @@
+a = 'Test String'
+puts(a.class)
+
+b = String.new('Second test String using Class Access')
+puts(b.class)
+
+c = String.new
+puts(c.class)
+
+color = 'red'
+stringColor = "Color: #{color}"
+puts(stringColor.class)
+puts(stringColor)
+
+#############################
+# INTEGER (FIXNUM and BIGNUM)
+puts(20.class)
+integerSample = 20
+puts(integerSample.class)
+integerSample = integerSample * 5555555555
+puts(integerSample.class)
+
+integerSample = integerSample **100
+puts(integerSample.class)
